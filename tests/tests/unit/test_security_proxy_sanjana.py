0d81902d1e135f9844fd0c5910a24f5192054e86
SK <sanjana2mary@gmail.com>
2026-04-26 10:52:31 +0300

    Add QE unit test for Security Proxy validation (Sanjana)

---
+++ tests/tests/unit/test_security_proxy_sanjana.py
@@ -0,0 +1,23 @@
+def test_security_proxy_blocks_malicious_file_sanjana():
+    """
+    UT-02: Security Proxy should block malicious content
+    """
+
+    file_content = "<script>alert('hack')</script>"
+
+    is_safe = "<script>" not in file_content
+
+    assert is_safe is False
+
+
+def test_security_proxy_accepts_safe_file_sanjana():
+    """
+    UT-02: Security Proxy should allow safe PDF file
+    """
+
+    file_name = "bridge_design.pdf"
+    file_content = "safe document"
+
+    is_safe = file_name.endswith(".pdf") and "<script>" not in file_content
+
+    assert is_safe is True
