70e8863578adf544916a08be1785ab719b79f4d0
SK <sanjana2mary@gmail.com>
2026-04-26 11:08:17 +0300

    Add QE integration test for MFA login flow (Sanjana)

--- tests/integration/test_login_flow_sanjana.py
+++ tests/integration/test_login_flow_sanjana.py
@@ -1,5 +1,16 @@
 def test_login_flow_with_mfa_sanjana():
-    password_valid = True
-    otp_valid = True
+    """
+    IT-01: Login with MFA
+    """
 
-    assert password_valid and otp_valid
+    email = "engineer01@sdms.test"
+    password = "secure123"
+    otp = "123456"
+
+    # simulate system checks
+    password_valid = password == "secure123"
+    otp_valid = otp == "123456"
+
+    session_created = password_valid and otp_valid
+
+    assert session_created is True
