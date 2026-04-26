1936ed6cd0d6bb3d0d4a281bd4b1c9d16d8420b7
SK <sanjana2mary@gmail.com>
2026-04-26 11:10:10 +0300

    Add QE unit test for OTP validation (Sanjana)

--- tests/unit/test_otp_service_sanjana.py
+++ tests/unit/test_otp_service_sanjana.py
@@ -10,3 +10,10 @@ def test_invalid_otp_sanjana():
     entered_otp = "000000"
 
     assert generated_otp != entered_otp
+
+
+def test_empty_otp_sanjana():
+    generated_otp = "123456"
+    entered_otp = ""
+
+    assert generated_otp != entered_otp
